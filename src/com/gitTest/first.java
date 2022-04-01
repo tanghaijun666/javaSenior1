64c781eb4f7405528c3b63ad5b33a40e5e7dc05c
thj <thj.666@qq.com>
2022-04-01 15:37:05 +0800

    second commit

---
+++ src/com/gitTest/first.java
@@ -0,0 +1,18 @@
+package com.gitTest;
+
+/**
+ * @author 彼此
+ * @create 2022-04-01 15:08
+ */
+public class first {
+    public static void main(String[] args) {
+        System.out.println("the first ");
+        System.out.println("你不是好人");
+        System.out.println("你不是好人");
+        System.out.println("你不是好人");
+        System.out.println("你不是好人");
+        System.out.println("你不是好人");
+        System.out.println("你不是好人");
+
+    }
+}
